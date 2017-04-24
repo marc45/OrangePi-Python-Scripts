a2655c62fa6f89e2723a61fd08348b7c92276977
Michael Landry <michaellandry3@hotmail.com>
2017-04-24 01:32:50 -0300

    Basic controller for 2 Relays & one sensor
    Turns fan on & dry heat source off when temp reaches 39, turns on dry heat source and turns off fan when temperature reaches 37. This maintains temperature but does not manage humidity which will be the next step.

---
+++ scripts/IncubatorController.py
@@ -0,0 +1,48 @@
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+import dht11
+import time
+import datetime
+
+
+# initialize GPIO
+#gpio.setwarnings(False)
+#gpio.setmode(GPIO.BCM)
+#gpio.cleanup()
+# Set relay pins as output
+#gpio.setup(0, GPIO.OUT)
+gpio.init()
+PIN = port.PA20
+gpio.setcfg(port.PA20, gpio.OUTPUT)
+gpio.setcfg(port.PC4, gpio.OUTPUT)
+gpio.setcfg(port.PC7, gpio.OUTPUT)
+gpio.output(port.PC4, gpio.HIGH)
+gpio.output(port.PC7, gpio.HIGH)
+#
+# read data using pin 20
+instance = dht11.DHT11(pin=PIN)
+Fan = 0
+Heat = 0
+while True:
+    result = instance.read()
+    if (result.temperature) == 0:
+      continue 
+    tempHI = 39
+    tempLOW = 37
+    print "Temp n Humidity Results:", result.temperature, result.humidity
+    DATA = [ result.temperature, result.humidity ]  
+    if (result.temperature) >= tempHI and Fan == 0:
+      print "Detected High Temp, fan is not activated, Turning on fan and turning off light if active"
+      gpio.setcfg(port.PC7, gpio.OUTPUT)
+      gpio.output(port.PC7, gpio.LOW) 
+      gpio.output(port.PC4, gpio.HIGH)
+      Fan = 1
+      Heat = 0
+    if (result.temperature) <= tempLOW and (result.temperature) != 0 and Heat != 1:
+      print "Detected Low Temp,turning off fan and turning heat source on"
+      gpio.setcfg(port.PC7, gpio.OUTPUT)
+      gpio.output(port.PC7, gpio.HIGH)
+      gpio.output(port.PC4, gpio.LOW)
+      Fan = 0 
+      Heat = 1
+    time.sleep(5)
